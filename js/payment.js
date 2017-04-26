5c7be4d5dd5f05c56932ccc5b879f22ad9f344dd
option0417 <you@example.com>
2017-04-26 05:44:41 +0000

    Add event handler to input-element for onchage and onfocus.

--- js/payment.js
+++ js/payment.js
@@ -1,7 +1,23 @@
+var elementItemPrice;
+var elementAmount;
+var elementTotalCost;
+
 function pageload() {
-  var e1 = document.getElementById('id_item_type');
+  var e1 = document.getElementById("id_item_type");
   console.log(e1);
   console.log(e1.options);
   console.log(e1.selectedIndex);
+
+	elementItemPrice = document.getElementById("id_item_price");
+	elementAmount = document.getElementById("id_amount");
+	elementTotalCost = document.getElementById("id_total_cost");
+
+	elementItemPrice.value = 0;
+	elementAmount.value = 1;
+	elementTotalCost.value = 0;
 }
 window.onload = pageload;
+
+function caculateTotalCost() {
+	elementTotalCost.value = elementItemPrice.value * elementAmount.value;
+}
